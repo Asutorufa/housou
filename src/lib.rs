596bfdea8e999387662180dc00121bdc5a3b4e87
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2026-02-19 00:34:26 +0800

    f
    Signed-off-by: Asutorufa <16442314+Asutorufa@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -202,7 +202,9 @@ async fn router(req: Request, env: Env) -> Result<Response> {
 
         // Handle Options for CORS on auth routes
         (Method::Options, path)
-            if path.starts_with("/api/auth") || path.starts_with("/api/user") =>
+            if path.starts_with("/api/auth")
+                || path.starts_with("/api/user")
+                || path.starts_with("/api/metadata") =>
         {
             Response::empty()
         }
